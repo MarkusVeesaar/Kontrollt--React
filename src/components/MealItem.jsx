ea46ceb614411600be3bc03aa65fe8375a30234f
MarkusVeesaar <Markus.veesaar@voco.ee>
2026-03-20 14:55:30 +0200

    Ülesanne 5 - Valuuta andmete kuvamine

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,13 +1,16 @@
 import "../index.css";
 
 const MealItem = (props) => {
+    const Hind = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(
+        props.meal.price
+    );
     return (
         <li>
             <article className="meal-item">
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p>{props.meal.price}</p>
+                    <p>{Hind}</p>
                     <p>{props.meal.description}</p>
                 </div>
                 <p>
